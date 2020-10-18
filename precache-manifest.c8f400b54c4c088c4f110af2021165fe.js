8226cd863f72a0c04ddf8687f473ab4929759bd2
scasals808 <54152638+scasals808@users.noreply.github.com>
2020-10-18 19:43:07 +0700

    Updates

---
+++ precache-manifest.c8f400b54c4c088c4f110af2021165fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc425ca85c17cf38451478be2d9f5ae8",
+    "url": "/myPortfolio/index.html"
+  },
+  {
+    "revision": "8b76511d045c842da83d",
+    "url": "/myPortfolio/static/css/main.9f1e019f.chunk.css"
+  },
+  {
+    "revision": "8343cd71869a07d380d0",
+    "url": "/myPortfolio/static/js/2.b4b604e9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/myPortfolio/static/js/2.b4b604e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b76511d045c842da83d",
+    "url": "/myPortfolio/static/js/main.22cd6d0f.chunk.js"
+  },
+  {
+    "revision": "0f713a185793586653f3",
+    "url": "/myPortfolio/static/js/runtime-main.d351083c.js"
+  }
+]);
